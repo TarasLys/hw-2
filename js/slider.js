3cc0b3e604e781fc0336579d7f04c1e78e66b72e
TarasLys <opros0609@gmail.com>
2023-07-11 22:50:15 +0300

    777

---
+++ js/slider.js
@@ -0,0 +1,33 @@
+$('.autoplay').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dotsClass: "my-dots",
+    autoplay: true,
+    autoplaySpeed: 200000,
+});
+
+// $('.autoplay').slick({
+//     dots: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     // dotsClass: "none",
+//     autoplay: true,
+//     autoplaySpeed: 3000,
+// });
+
+
+// $('.fade').slick({
+//   dots: false,
+//   infinite: true,
+//   speed: 500,
+//   fade: true,
+//   cssEase: 'linear'
+// });
+
+// $('.autoplay').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
